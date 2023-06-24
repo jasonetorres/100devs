87cc1bd77bb9246c56cc86420a44e3175a2715ec
Jason Torres <jasontorres585@icloud.com>
2023-06-24 07:25:07 -0400

    updated JS

--- class 12/background-picker/js/main.js
+++ class 12/background-picker/js/main.js
@@ -1,24 +1,8 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
-document.getElementById('red').onclick = partyRed
+document.getElementById('bluepill').onclick = function() {
+  window.location.href = 'https://www.google.com';
+};
 
+document.getElementById('redpill').onclick = function() {
+  window.location.href = 'animation.html';
+};
 
-function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
-}
-
-function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
-}
-
-function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'black'
-}
-function partyRed() {
-  document.querySelector('body').style.backgroundColor= 'rgba(242, 7,7)'
-  document.querySelector('body').style.color= 'black'
-}
